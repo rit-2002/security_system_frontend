aa781d466be35c59568753888f022d5da5060c12
rit-2002 <128469291+rit-2002@users.noreply.github.com>
2025-03-20 09:29:32 +0530

    start to integrate login api

---
+++ security-system/src/apis/authAPI.js
@@ -0,0 +1,23 @@
+import { axiosAPI } from "./axiosInstance";
+
+const API_BASE_URL = '/auth';
+
+export const login = async (username, password, captchaValue) => {
+  try {
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+ 
+
+    const response = await axiosAPI.post(`${API_BASE_URL}/login/token`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.error('Error logging in:', error);
+    throw error;
+  }
+};
