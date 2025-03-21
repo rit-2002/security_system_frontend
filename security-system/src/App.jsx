793cafee47d73ff648f6a6b0bb6cbe00c2787f09
rit-2002 <128469291+rit-2002@users.noreply.github.com>
2025-03-21 08:50:05 +0530

    created new appProvider

---
+++ security-system/src/App.jsx
@@ -0,0 +1,14 @@
+
+import { AppProvider } from './contexts/AppProvider'
+import AppRoutes from './routes/AppRoutes'
+
+
+function App() {
+  return (
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
+  )
+}
+
+export default App
